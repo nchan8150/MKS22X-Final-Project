a36e1c598ce7fcee0357833613e35be61633e1a9
jsun5 <jsun5@stuy.edu>
2018-06-04 14:49:24 -0400

    writing entity and entity coord skeleton for use by enemies, need to learn color change

---
+++ Entity.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public abstract class Entity implements Tickable{
+	
+	private EntityCoordinate position;
+	private int size;
+	private Color color;
+	private boolean alive = true;
+	
+//	public Entity(EntityCOordinate position){
+//		this
+}
---
+++ EntityCoordinate.java
@@ -0,0 +1,21 @@
+import java.util.List;
+
+public class EntityCoordinate{
+	private double x;
+	private double y;
+
+		
+	public EntityCoordinate(double x, double y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double getX(){
+		return x;
+	}
+	
+	public double getY(){
+		return y;
+	}
+}
+	

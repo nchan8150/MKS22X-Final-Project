eca790d3b06315fe7d4e8652ae6e3fa71dbb69ba
nchan8150 <nchan1@stuy.edu>
2018-06-03 22:18:33 -0400

    finished linear projectile

---
+++ LinearProjectile.java
@@ -0,0 +1,12 @@
+public class LinearProjectile extends Projectile {
+
+	public LinearProjectile(Turret source, EntityCoordinate position, VelocityVector velocity) {
+		super(source, position);
+		super.setVelocityVector(velocity);
+	}
+	
+	@Override
+	public void tick() {
+		move();
+	}
+}

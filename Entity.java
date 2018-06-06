8f41493032ff3c5084d3ba3a193f3658b4a081c3
jsun5 <jsun5@stuy.edu>
2018-06-05 20:15:47 -0400

    incorporated VelocityVector for mvmt (wrote by nick) and wrote proper get/set methods

--- Entity.java
+++ Entity.java
@@ -4,10 +4,59 @@ import java.util.List;
 public abstract class Entity implements Tickable{
 	
 	private EntityCoordinate position;
+	private VelocityVector velocity;
 	private int size;
 	private Color color;
 	private boolean alive = true;
 	
-//	public Entity(EntityCOordinate position){
-//		this
+	public Entity() {
+		this(new EntityCoordinate(0, 0));
+	}
+	public Entity(EntityCoordinate position) {
+		this(position, 20);
+	}
+	public Entity(EntityCoordinate position, int size) {
+		this(position, size, Color.BLACK);
+	}
+	public Entity(EntityCoordinate position, int size, Color color) {
+		this.position = position;
+		this.size = size;
+		this.color = color;
+		this.velocity = new VelocityVector(0, 0);
+	}
+	
+		public Color getColor() {
+		return color;
+	}
+	public void setColor(Color color) {
+		this.color = color;
+	}
+	public EntityCoordinate getPosition() {
+		return position;
+	}
+	public double getX() {
+		return position.getX();
+	}
+	protected void setX(double x) {
+		if (x >= 0 && x <= Map.COLUMNS)
+			position.setX(x);
+		if (x < 0)
+			position.setX(0);
+		if (x > Map.COLUMNS)
+			position.setX(Map.COLUMNS);
+	}
+	public double getY() {
+		return position.getY();
+	}
+	protected void setY(double y) {
+		if (y >= 0 && y <= Map.ROWS)
+			position.setY(y);
+		if (y < 0)
+			position.setY(0);
+		if (y > Map.ROWS)
+			position.setY(Map.ROWS);
+	}
+	public int getSize() {
+		return size;
+	}
 }
